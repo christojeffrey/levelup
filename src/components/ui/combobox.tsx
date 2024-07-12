a42b5f04ceb557e65f74319c216026768ec68e7d
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-07-12 16:14:34 +0700

    feat: onboarding

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -5,65 +5,46 @@ import { Check, ChevronsUpDown } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { useState, useEffect } from "react";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
+export default function Combobox({ details, onChange, withSearch = false }) {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
 
-export default function Combobox() {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  useEffect(() => {
+    onChange(value);
+  }, [value]);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button variant="outline" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
-          {value ? frameworks.find((framework) => framework.value === value)?.label : "Select framework..."}
+        <Button variant="outline" role="combobox" aria-expanded={open} className="w-full justify-between">
+          {value ? details.find((detail) => detail.value === value)?.label : "Select one..."}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full p-0">
         <Command>
-          <CommandInput placeholder="Search framework..." />
-          <CommandEmpty>No framework found.</CommandEmpty>
+          {withSearch && <CommandInput placeholder="Search..." />}
+          <CommandEmpty>No found.</CommandEmpty>
           <CommandGroup>
-            {frameworks.map((framework) => {
-              console.log(framework);
-              return (
+            <CommandList>
+              {details.map((detail) => (
                 <CommandItem
-                  key={framework.value}
-                  value={framework.value}
+                  key={detail.value}
+                  value={detail.value}
                   onSelect={(currentValue) => {
                     setValue(currentValue === value ? "" : currentValue);
                     setOpen(false);
                   }}
                 >
-                  {/* <Check className={cn("mr-2 h-4 w-4", value === framework.value ? "opacity-100" : "opacity-0")} />
-                {framework.label} */}
+                  <Check className={cn("mr-2 h-4 w-4", value === detail.value ? "opacity-100" : "opacity-0")} />
+                  {detail.label}
                 </CommandItem>
-              );
-            })}
+              ))}
+            </CommandList>
           </CommandGroup>
         </Command>
       </PopoverContent>
