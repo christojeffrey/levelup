be281da99082271ce1f5606d9b04259a17db881d
Adityapnn811 <apn725@gmail.com>
2024-07-12 17:15:14 +0700

    feat: leaderboard

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -15,32 +15,46 @@ const ranks = [
 ];
 
 export default function Leaderboard() {
-  const [currentRank, setCurrentRank] = useState(3);
+  const [currentRank, setCurrentRank] = useState(0);
   return (
-    <main className="m-2 min-h-full">
-        <nav>&lt;</nav>
-      <h1 className="text-bold">Leaderboard</h1>
-      <section className="flex flex-1 justify-center">
-        <ul className="flex flex-col gap-3 justify-center">
+    <main className="my-2 mx-4">
+      <a className="text-bold" href={`/`}>&lt;</a>
+      <div>
+        <h1 className="text-bold text-2xl mx-2 my-2">Leaderboard</h1>
+        <div className="mx-2 flex flex-row align-middle gap-3 my-2">
+          <div className="rounded-full w-8 h-8 bg-[#00DBCE]"></div>
+          <span className="my-auto">Diamond Tier</span>
+        </div>
+      </div>
+      <section className="flex flex-1 justify-center mt-3">
+        <ul className="flex flex-col gap-3 justify-center w-4/5">
           {ranks.map((rank, index) => {
-            let scale = 100;
             let className = "";
             if (index === currentRank) {
-              scale = 125;
-              className = "bg-[#3C7A6F] scale-" + scale;
-            } else if (index - 1 === currentRank || index + 1 === currentRank ) {
-              scale = 110;
-              className = "bg-[#52988D] scale-" + scale;
-            } else if (index - 2 === currentRank || index + 2 === currentRank ) {
-              scale = 105;
-              className = "bg-[#76BFA1] scale-" + scale;
+              className = `bg-[#3C7A6F] scale-[1.15]`;
+            } else if (index - 1 === currentRank || index + 1 === currentRank) {
+              className = `bg-[#52988D] scale-[1.10]`;
+            } else if (index - 2 === currentRank || index + 2 === currentRank) {
+              className = `bg-[#76BFA1] scale-[1.05]`;
             }
             return (
               <li
                 key={index}
-                className={`${className} border-2 text-center`}
+                className={`flex rounded-2xl px-4 py-3 flex-row align-middle justify-between ${
+                  className === "" ? "bg-[#E6E4D7]" : className
+                }`}
               >
-                {rank.name} - {rank.score}
+                <span className="flex flex-row align-middle">
+                  <span className="my-auto mr-2">{index + 1}</span>
+                  <img
+                    src="/profile.webp"
+                    width={32}
+                    height={32}
+                    className="rounded-full mr-1"
+                  ></img>
+                  <span className="my-auto">{rank.name}</span>
+                </span>
+                <span className="my-auto">{rank.score}</span>
               </li>
             );
           })}
